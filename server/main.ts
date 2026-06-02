042377d9b1598b831fd5001ae7f7c8b79ad0a6c9
Sean Freeman <sfreeman123456789@gmail.com>
2026-06-02 20:27:20 +0530

    setServerTimeout(60) to prevent Railway edge 504

--- server/main.ts
+++ server/main.ts
@@ -1,4 +1,7 @@
-import { onRequest } from "mates-fullstack";
+import { onRequest, setServerTimeout } from "mates-fullstack";
+
+// Give requests up to 60 seconds (slow SQLite under load)
+setServerTimeout(60);
 
 onRequest((c) => {
   c.resHeaders["x-benchmark"] = "1";
